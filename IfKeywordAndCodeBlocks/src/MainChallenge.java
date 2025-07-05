1f07ce807b022be4216c5a92d6270356e13270fb
Sushrut Vinayak <sushrutvinayakofficial@gmail.com>
2025-07-05 14:28:38 +0530

    IfKeywordAndCodeBlocks Added

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,16 +1,25 @@
 public class MainChallenge {
 
     public static void main(String[] args) {
+
         boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The Highscore is " + highScore);
+        System.out.println("The Highscore is " + calculateScore(true, 10000, 8, 200));
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " +  finalScore);
+            finalScore += 1000;
         }
 
+        return finalScore;
     }
 }
