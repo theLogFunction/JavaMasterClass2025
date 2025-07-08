db6d197b45c18f87296b277b6139b68c1d2ae057
Sushrut Vinayak <sushrutvinayakofficial@gmail.com>
2025-07-08 08:14:37 +0530

    IfKeywordAndCodeBlocks Added

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -10,6 +10,8 @@ public class MainChallenge {
         int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
         System.out.println("The Highscore is " + highScore);
         System.out.println("The Highscore is " + calculateScore(true, 10000, 8, 200));
+
+        displayHighScorePosition("Ganesh", calculateHighScorePosition(600));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -21,5 +23,21 @@ public class MainChallenge {
         }
 
         return finalScore;
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into position: " + playerPosition);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+
+        int position = 4;
+        if(score >= 1000)
+            position = 1;
+        else if(score >= 500)
+            position = 2;
+        else if(score >= 100)
+            position = 3;
+        return position;
     }
 }
