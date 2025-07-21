d99384266aa23b6675740f6371c6c8b7acbb0407
Sushrut Vinayak <sushrutvinayakofficial@gmail.com>
2025-07-21 14:03:13 +0530

    Nato Usage Challenge

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -2,11 +2,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int switchValue = 1;
+        System.out.println(getQuarter("Jan"));
+        System.out.println(getNatoUsage('A'));
+    }
 
-        switch (switchValue) {
-            case 1 -> System.out.println("Value is 1");
-            default -> System.out.println("Not 1");
+    public static String getQuarter(String month) {
+
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "First";
+            case "APRIL", "MAY", "JUNE" -> "Second";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "Third";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "Fourth";
+            default -> "Bad Month";
+        };
+    }
+
+    public static String getNatoUsage(char letter) {
+
+        String natoWord;
+        switch (letter) {
+            case 'A' -> natoWord = "able";
+            case 'B' -> natoWord = "baker";
+            case 'C' -> natoWord = "charlie";
+            case 'D' -> natoWord = "dog";
+            case 'E' -> natoWord = "easy";
+            default -> natoWord = "Not Found";
         }
+
+        return "Nato Word was " + natoWord;
     }
 }
